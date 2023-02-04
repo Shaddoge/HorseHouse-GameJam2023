beac12ae82c5ba23ea5c9af2e1f07e38886cf96c
EM-Neon <blindsuneclipse@gmail.com>
2023-02-05 03:54:57 +0800

    added spawnerManager and edited other enemies to have navMesh

---
+++ HorseHouse-GameJam/Assets/Dev/Scripts/SpawnerManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] spawners;
+    private int levelCount = 0;
+
+
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        EventManager.Instance.isTransitioning += IsTransitioning;
+    }
+    private void OnDisable()
+    {
+        EventManager.Instance.isTransitioning -= IsTransitioning;
+    }
+
+    // Update is called once per frame
+    void Start()
+    {
+        foreach(var spawner in spawners)
+        {
+            spawner.SetActive(false);
+        }
+
+        spawners[0].SetActive(true);
+    }
+
+    public void IsTransitioning()
+    {
+        GameObject[] allEnemies;
+
+        allEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        foreach(GameObject enemy in allEnemies)
+        {
+            Destroy(enemy);
+        }
+
+        spawners[levelCount].SetActive(false);
+
+        levelCount++;
+
+        spawners[levelCount].SetActive(true);
+    }
+}
