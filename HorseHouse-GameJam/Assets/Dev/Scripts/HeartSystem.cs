3371ad7617cf6bac1b72d8da1881e0717941d933
CesarManalac <cesar_manalac@dlsu.edu.ph>
2023-02-05 09:19:23 +0800

    updated main game ui to change with the eras

--- HorseHouse-GameJam/Assets/Dev/Scripts/HeartSystem.cs
+++ HorseHouse-GameJam/Assets/Dev/Scripts/HeartSystem.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class HeartSystem : MonoBehaviour
 {
     public Image[] hearts;
+    public Sprite[] heartAssets;
 
     private void Start()
     {
@@ -15,17 +16,40 @@ public class HeartSystem : MonoBehaviour
     private void OnEnable()
     {
         EventManager.Instance.updateHealth += UpdateHealth;
+        EventManager.Instance.changeHealthIcon+= ChangeHealthIcon;
     }
     private void OnDisable()
     {
         EventManager.Instance.updateHealth -= UpdateHealth;
+        EventManager.Instance.changeHealthIcon -= ChangeHealthIcon;
     }
 
     void Update()
     {
-
     }
-    
+    private void ChangeHealthIcon()
+    {
+        if (GameManager.Instance.CurrentEra == Era.Space)
+        {
+            for (int i = 0; i < hearts.Length; i++)
+            {
+                hearts[i].sprite = heartAssets[0];
+            }
+        }
+        else if(GameManager.Instance.CurrentEra == Era.Modern) {
+            for(int i = 0; i < hearts.Length; i++)
+            {
+                hearts[i].sprite = heartAssets[1];
+            }
+        }
+        else if (GameManager.Instance.CurrentEra == Era.Stone)
+        {
+            for (int i = 0; i < hearts.Length; i++)
+            {
+                hearts[i].sprite = heartAssets[2];
+            }
+        }
+    }
     public void UpdateHealth(int health)
     {
         Debug.Log(health);
